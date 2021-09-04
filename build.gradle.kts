cf2137b18ca6eeb14ac5e6efe7102ae00b882882
NotMyFault <mc.cache@web.de>
2021-09-04 12:32:07 +0200

    Back to snapshot for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ the<JavaPluginExtension>().toolchain {
 }
 
 group = "com.intellectualsites.paster"
-version = "1.1.1"
+version = "1.1.2"
 var versuffix by extra("SNAPSHOT")
 version = if (!project.hasProperty("release")) {
     String.format("%s-%s", project.version, versuffix)
